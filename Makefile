6b4192e1285f250a85aa18b96503c34f2ab1cf5e
rt0112358 <rt0112358@gmail.com>
2021-02-12 12:43:34 -0700

    tkinter

--- Makefile
+++ Makefile
@@ -22,3 +22,6 @@ push:
 	git commit -m "$(msg)"
 	@git status
 	@git push
+
+gui:
+	python3 tkinter/hello_world.py
---
+++ tkinter/hello_world.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+import tkinter.messagebox
+#########################################
+# Hello Tkinter World.
+#
+# Usage:
+#   $ python3 hello_tk_world.py
+# Makefile:
+#   $ make gui
+#########################################
+WIDTH = 142
+
+# Starts the party by creating a tkinter ojbect,
+# and returning it as the variable called "gui"
+def init():
+    gui = tk.Tk()
+    gui.title('hello world')
+    gui.geometry("240x320")
+    return gui
+
+# Creates a tkinter message box 
+def hello_world():
+    tk.messagebox.showinfo("???", "Hello Tkinter World!")
+
+# Tkinter button handler function
+def button_click(gui_frame):
+    button = tk.Button(gui_frame, text="Say Hi", width=WIDTH, command=hello_world)
+    button.pack()
+
+# Exits and ends the entire program
+def die(gui_frame):
+    exit_button = tk.Button(gui_frame, text="Exit", width=WIDTH, command=gui_frame.destroy)
+    exit_button.pack()
+
+# Main driver function
+def main():
+    framework = init()
+    button_click(framework)
+    die(framework)
+    framework.mainloop()
+
+# Calling the main function
+main()
